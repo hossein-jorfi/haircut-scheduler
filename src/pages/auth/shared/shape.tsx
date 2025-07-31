e0386f1e5b443db13553bafd2e69e9d2a3d6a095
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-07-31 17:28:02 +0330

    Shape

--- src/pages/auth/shared/shape.tsx
+++ src/pages/auth/shared/shape.tsx
@@ -2,7 +2,7 @@ import BarberShopShape from "@/assets/images/barbershop.svg";
 
 const Shape = () => {
   return (
-    <div className="relative hidden md:flex md:items-center md:justify-center w-[350px] lg:w-[450px] p-5">
+    <div className="relative hidden md:flex md:items-center md:justify-center w-[350px] lg:w-[400px] p-5">
       {/* <div className="bg-primary/5 pattern-1 h-full w-full rounded-md border"></div> */}
       <img src={BarberShopShape} alt="shape" className="h-full w-full" />
     </div>

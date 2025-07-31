45a397717c09a4261df5ddaf8fa20101cdb362de
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-07-31 14:01:22 +0330

    login form

--- src/pages/auth/login/login-form.tsx
+++ src/pages/auth/login/login-form.tsx
@@ -1,8 +1,9 @@
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
-import { Card, CardContent } from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { ScissorsIcon } from "lucide-react";
 
 export function LoginForm({
   className,
@@ -15,14 +16,16 @@ export function LoginForm({
           <form className="p-6 md:p-8">
             <div className="flex flex-col gap-6">
               <div className="flex flex-col items-center text-center">
-                <h1 className="text-2xl font-bold">Welcome back</h1>
+                <h1 className="text-2xl font-bold">ورود</h1>
                 <p className="text-muted-foreground text-balance">
-                  Login to your Acme Inc account
+                  ورود به حساب کاربری
                 </p>
               </div>
               <div className="grid gap-3">
-                <Label htmlFor="email">Email</Label>
+                <Label htmlFor="email">ایمیل</Label>
                 <Input
+                  dir="ltr"
+                  lang="en"
                   id="email"
                   type="email"
                   placeholder="m@example.com"
@@ -30,23 +33,21 @@ export function LoginForm({
                 />
               </div>
               <div className="grid gap-3">
-                <div className="flex items-center">
-                  <Label htmlFor="password">Password</Label>
-                  <a
-                    href="#"
-                    className="ml-auto text-sm underline-offset-2 hover:underline"
-                  >
-                    Forgot your password?
-                  </a>
+                <div className="flex items-center justify-between">
+                  <Label htmlFor="password">رمز عبور</Label>
+
+                  <Button variant="link" className="p-0 m-0 h-fit">
+                    رمز عبور خود را فراموش کرده اید؟
+                  </Button>
                 </div>
                 <Input id="password" type="password" required />
               </div>
               <Button type="submit" className="w-full">
-                Login
+                ورود
               </Button>
               <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
                 <span className="bg-card text-muted-foreground relative z-10 px-2">
-                  Or continue with
+                  یا
                 </span>
               </div>
               <div className="grid grid-cols-3 gap-4">
@@ -57,7 +58,7 @@ export function LoginForm({
                       fill="currentColor"
                     />
                   </svg>
-                  <span className="sr-only">Login with Apple</span>
+                  <span className="sr-only">ورود با Apple</span>
                 </Button>
                 <Button variant="outline" type="button" className="w-full">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
@@ -79,26 +80,22 @@ export function LoginForm({
                 </Button>
               </div>
               <div className="text-center text-sm">
-                Don&apos;t have an account?{" "}
-                <a href="#" className="underline underline-offset-4">
-                  Sign up
-                </a>
+                حساب کاربری ندارید؟{" "}
+                <Button variant="link" className="p-0 m-0 h-fit">
+                  ثبت نام
+                </Button>
               </div>
             </div>
           </form>
-          <div className="bg-muted relative hidden md:block">
-            <img
-              src="/placeholder.svg"
-              alt="Image"
-              className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-            />
+          <div className="bg-muted relative hidden md:flex md:items-center md:justify-center w-[350px]">
+            <ScissorsIcon className="size-1/2" />
           </div>
         </CardContent>
       </Card>
       <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
-        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
-        and <a href="#">Privacy Policy</a>.
+        با کلیک بر روی ادامه، شما با <a href="#">قوانین و مقررات</a> و{" "}
+        <a href="#">حریم خصوصی</a> موافق هستید.
       </div>
     </div>
-  )
+  );
 }

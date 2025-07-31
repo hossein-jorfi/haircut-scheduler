99bb03b30427b53cdfa1994effb4541b873d4038
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-07-31 12:25:31 +0330

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div className="text-xl m-30 flex gap-3">
+      <Button>Button</Button>
+      <Button variant="destructive">Button</Button>
+      <Button variant="ghost">Button</Button>
+      <Button variant="link">Button</Button>
+      <Button variant="outline">Button</Button>
+      <Button variant="secondary">Button</Button>
+    </div>
+  );
+}
+
+export default App;

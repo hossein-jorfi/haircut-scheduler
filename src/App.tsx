efd3b24eadc81307f6b23b7d7c88d9d833ec0f77
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-07-31 12:28:16 +0330

    theme

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,11 @@ function App() {
   return (
     <div className="text-xl m-30 flex gap-3">
       <Button>Button</Button>
-      <Button variant="destructive">Button</Button>
-      <Button variant="ghost">Button</Button>
-      <Button variant="link">Button</Button>
-      <Button variant="outline">Button</Button>
       <Button variant="secondary">Button</Button>
+      <Button variant="outline">Button</Button>
+      <Button variant="link">Button</Button>
+      <Button variant="ghost">Button</Button>
+      <Button variant="destructive">Button</Button>
     </div>
   );
 }

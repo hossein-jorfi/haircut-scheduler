39d973e64b6c447b4189aa2f07c8fc39566b32d3
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-07-31 17:26:48 +0330

    shared Shape

--- src/pages/auth/shared/shape.tsx
+++ src/pages/auth/shared/shape.tsx
@@ -1,7 +1,10 @@
+import BarberShopShape from "@/assets/images/barbershop.svg";
+
 const Shape = () => {
   return (
-    <div className="relative hidden md:flex md:items-center md:justify-center w-[350px] p-3">
-      <div className="bg-primary/5 pattern-1 h-full w-full rounded-md border"></div>
+    <div className="relative hidden md:flex md:items-center md:justify-center w-[350px] lg:w-[450px] p-5">
+      {/* <div className="bg-primary/5 pattern-1 h-full w-full rounded-md border"></div> */}
+      <img src={BarberShopShape} alt="shape" className="h-full w-full" />
     </div>
   );
 };

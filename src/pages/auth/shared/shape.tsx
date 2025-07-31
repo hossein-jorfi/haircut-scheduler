4705e27956f10980c92b36a6024073b8ac8b75c9
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-07-31 17:18:31 +0330

    styles

--- src/pages/auth/shared/shape.tsx
+++ src/pages/auth/shared/shape.tsx
@@ -1,7 +1,7 @@
 const Shape = () => {
   return (
     <div className="relative hidden md:flex md:items-center md:justify-center w-[350px] p-3">
-      <div className="bg-primary/5 pattern-1 h-full w-full rounded-lg border"></div>
+      <div className="bg-primary/5 pattern-1 h-full w-full rounded-md border"></div>
     </div>
   );
 };

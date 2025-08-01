49bdc2b66e19f475e7c6e5c16da1f68a80820c9c
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-08-01 12:00:57 +0330

    navigate user based on auth state

--- src/layout/dashboard/dashboard-layout.tsx
+++ src/layout/dashboard/dashboard-layout.tsx
@@ -1,6 +1,14 @@
+import useAuthStore from "@/store/auth/useAuthStore";
 import { type ReactNode } from "react";
+import { Navigate } from "react-router";
 
 const DashboardLayout = ({ children }: { children: ReactNode }) => {
+  const isLoggedIn = useAuthStore((state) => state.isLoggedIn);
+
+  if (!isLoggedIn) {
+    return <Navigate to="/auth/login" />;
+  }
+
   return (
     <div className="flex h-screen w-screen flex-col">
       <p>DashboardLayout</p>

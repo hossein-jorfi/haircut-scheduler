90bd7644fbc51942f7da2e3b18b531b998fe97ae
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-08-02 10:15:24 +0330

    ThemeToggle

--- src/layout/dashboard/header/index.tsx
+++ src/layout/dashboard/header/index.tsx
@@ -1,5 +1,6 @@
 import useAuthStore from "@/store/auth/useAuthStore";
 import NewAppointmentButton from "./new-appointment-button";
+import { ThemeToggle } from "@/components/theme-provider/theme-toggle";
 
 const Header = () => {
   const userInfo = useAuthStore((state) => state.userInfo);
@@ -7,7 +8,10 @@ const Header = () => {
   return (
     <div className="mb-5 py-5">
       <div className="app-container flex justify-between items-center">
-        <div>{userInfo?.username}</div>
+        <div className="flex items-center gap-2">
+          <div>{userInfo?.username}</div>
+          <ThemeToggle />
+        </div>
         <NewAppointmentButton />
       </div>
     </div>

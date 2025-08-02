ccd8e94ec66bd93af6596fbcb1b62c462d9381db
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-08-02 10:04:24 +0330

    ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import { RouterProvider } from "react-router-dom";
 import router from "./routes";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "./components/ui/sonner";
+import { ThemeProvider } from "./components/theme-provider";
 
 const queryClient = new QueryClient();
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+        <RouterProvider router={router} />
 
-      <Toaster />
+        <Toaster />
+      </ThemeProvider>
     </QueryClientProvider>
   );
 }

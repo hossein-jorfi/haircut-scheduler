371e3a64449ff02c8b566d43bd28fd20fe9fa56a
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-08-04 15:50:26 +0330

    UserDropdown

--- src/layout/dashboard/header/index.tsx
+++ src/layout/dashboard/header/index.tsx
@@ -1,37 +1,13 @@
-import useAuthStore from "@/store/auth/useAuthStore";
 import NewAppointmentButton from "./new-appointment-button";
 import { ThemeToggle } from "@/components/theme-provider/theme-toggle";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { LogOut, User } from "lucide-react";
-import { Button } from "@/components/ui/button";
-import { useAuth } from "@/store/auth/hooks";
+import UserDropdown from "./user-dropdown";
 
 const Header = () => {
-  const userInfo = useAuthStore((state) => state.userInfo);
-
-  const { logoutUser } = useAuth();
   return (
     <div className="mb-5 py-5">
       <div className="app-container flex justify-between items-center">
         <div className="flex items-center gap-2">
-          <DropdownMenu dir="rtl">
-            <DropdownMenuTrigger asChild>
-              <Button variant="outline" size="icon">
-                <User />
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="start">
-              <div className="p-2 border-b mb-1">{userInfo?.username}</div>
-              <DropdownMenuItem onClick={logoutUser}>
-                <LogOut /> خروج
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
+          <UserDropdown />
           <ThemeToggle />
         </div>
         <NewAppointmentButton />
---
+++ src/layout/dashboard/header/user-dropdown.tsx
@@ -0,0 +1,34 @@
+import useAuthStore from "@/store/auth/useAuthStore";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { LogOut, User } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { useAuth } from "@/store/auth/hooks";
+
+const UserDropdown = () => {
+  const userInfo = useAuthStore((state) => state.userInfo);
+
+  const { logoutUser } = useAuth();
+
+  return (
+    <DropdownMenu dir="rtl">
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <User />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="start">
+        <div className="p-2 border-b mb-1">{userInfo?.username}</div>
+        <DropdownMenuItem onClick={logoutUser}>
+          <LogOut /> خروج
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default UserDropdown;

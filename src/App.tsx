54d23a3f876f28ef5f49e1326bf38f446afb5d92
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-08-04 14:20:29 +0330

    DirectionProvider

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,19 @@ import router from "./routes";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "./components/ui/sonner";
 import { ThemeProvider } from "./components/theme-provider";
+import { DirectionProvider } from "@radix-ui/react-direction";
 
 const queryClient = new QueryClient();
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-        <RouterProvider router={router} />
+      <DirectionProvider dir="rtl">
+        <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+          <RouterProvider router={router} />
 
-        <Toaster />
-      </ThemeProvider>
+          <Toaster />
+        </ThemeProvider>
+      </DirectionProvider>
     </QueryClientProvider>
   );
 }

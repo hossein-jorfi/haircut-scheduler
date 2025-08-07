1216969d5764d9a816e3f71878fc4204e1afc54e
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-08-07 13:22:59 +0330

    EmptyList styles

--- src/components/shared/empty-list.tsx
+++ src/components/shared/empty-list.tsx
@@ -1,3 +1,4 @@
+import { PackageOpen } from "lucide-react";
 import { H3 } from "../ui/typography";
 
 const EmptyList = ({
@@ -8,8 +9,10 @@ const EmptyList = ({
   icon?: React.ReactNode;
 }) => {
   return (
-    <div className="flex flex-col items-center justify-center h-[50vh] gap-4">
-      {icon}
+    <div className="flex flex-col items-center justify-center h-[50vh] gap-4 text-muted-foreground">
+      <div className="[&_svg]:size-30">
+        {icon || <PackageOpen className="size-10" />}
+      </div>
       <H3 className="text-center">{text || "موردی یافت نشد"}</H3>
     </div>
   );

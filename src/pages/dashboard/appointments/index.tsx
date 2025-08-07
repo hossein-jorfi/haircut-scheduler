a5e71d09efc134b443fc635a9041209421c33111
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-08-07 13:33:58 +0330

    AppointmentsModal

--- src/pages/dashboard/appointments/index.tsx
+++ src/pages/dashboard/appointments/index.tsx
@@ -2,19 +2,32 @@ import PageHeading from "@/components/shared/page-heading";
 import EmptyList from "@/components/shared/empty-list";
 import { Button } from "@/components/ui/button";
 import { Plus } from "lucide-react";
+import { Dialog } from "@/components/ui/dialog";
+import AppointmentsModal from "@/components/shared/appointments-modal";
+import { useState } from "react";
 
 const Appointments = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <div>
-      <PageHeading action={{ label: "رزرو نوبت", onClick: () => {} }}>
+      <PageHeading
+        action={{
+          label: "رزرو نوبت",
+          onClick: () => setOpen(true),
+        }}
+      >
         نوبت ها
       </PageHeading>
       <EmptyList text="هنوز نوبتی رزرو نشده است">
-        <Button>
+        <Button onClick={() => setOpen(true)}>
           <Plus />
           رزرو نوبت
         </Button>
       </EmptyList>
+      <Dialog open={open} onOpenChange={setOpen}>
+        <AppointmentsModal />
+      </Dialog>
     </div>
   );
 };

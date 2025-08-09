fa222453ad861f98cb895ed87617a8f35bc2a2d4
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-08-09 12:28:55 +0330

    add token to interceptors

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,21 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "./components/ui/sonner";
 import { ThemeProvider } from "./components/theme-provider";
 import { DirectionProvider } from "@radix-ui/react-direction";
+import api from "./services/api";
+import useAuthStore from "./store/auth/useAuthStore";
 
 const queryClient = new QueryClient();
 function App() {
+  const isLoggedIn = useAuthStore((state) => state.isLoggedIn);
+  const accessToken = useAuthStore((state) => state.userInfo?.token);
+
+  api.interceptors.request.use((config) => {
+    if (isLoggedIn && accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  });
+
   return (
     <QueryClientProvider client={queryClient}>
       <DirectionProvider dir="rtl">

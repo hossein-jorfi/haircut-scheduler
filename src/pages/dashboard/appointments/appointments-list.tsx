11d4eff28052c3bccfd0c392fd7f937332d3946f
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-08-15 11:10:42 +0330

    AppointmentsList

--- src/pages/dashboard/appointments/appointments-list.tsx
+++ src/pages/dashboard/appointments/appointments-list.tsx
@@ -1,8 +1,16 @@
-import { Calendar, CalendarX2, Pencil, UserRound } from "lucide-react";
+import {
+  Calendar,
+  CalendarX2,
+  Check,
+  Clock,
+  NotebookPen,
+  UserRound,
+} from "lucide-react";
 import { type AppointmentType } from ".";
 import {
   Card,
   CardContent,
+  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
@@ -10,6 +18,7 @@ import {
 import { Small } from "@/components/ui/typography";
 import { useNumberFormat } from "@/hooks/use-number-format";
 import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge";
 
 const AppointmentsList = ({
   appointments,
@@ -19,12 +28,18 @@ const AppointmentsList = ({
   const { formatNumber } = useNumberFormat();
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      {appointments.map((appointment) => (
+      {appointments.map((appointment, index) => (
         <Card key={appointment.id}>
           <CardHeader>
             <CardTitle className="flex items-center gap-2">
-              <UserRound /> {appointment.barber_name}
+              <UserRound className="size-5" /> {appointment.barber_name}
             </CardTitle>
+            <CardDescription className="mt-1">
+              <Badge variant={index % 2 === 0 ? "default" : "outline"}>
+                {index % 2 === 0 ? <Check /> : <Clock />}
+                {index % 2 === 0 ? "تایید شده" : "در انتظار تایید"}
+              </Badge>
+            </CardDescription>
           </CardHeader>
           <CardContent>
             <Small className="flex items-center gap-2">
@@ -34,7 +49,7 @@ const AppointmentsList = ({
           </CardContent>
           <CardFooter className="grid grid-cols-2 gap-2">
             <Button>
-              <Pencil />
+              <NotebookPen />
               تغییر تاریخ
             </Button>
             <Button variant="secondary">
